9b4c770edc400e1d27c1719acbd467d09adfd34a
Ali Mustafa <alimustafafortesting@gmail.com>
2023-12-19 09:34:18 +0200

    fourht push

---
+++ src/bookstore.HttpApi/Controllers/bookstoreController.cs
@@ -0,0 +1,14 @@
+﻿using bookstore.Localization;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace bookstore.Controllers;
+
+/* Inherit your controllers from this class.
+ */
+public abstract class bookstoreController : AbpControllerBase
+{
+    protected bookstoreController()
+    {
+        LocalizationResource = typeof(bookstoreResource);
+    }
+}
