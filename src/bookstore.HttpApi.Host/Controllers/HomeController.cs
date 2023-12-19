e520fc6a06ca1b711180648a0a6dc1e498b6245f
Ali Mustafa <alimustafafortesting@gmail.com>
2023-12-19 10:06:44 +0200

    Final one

---
+++ src/bookstore.HttpApi.Host/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace bookstore.Controllers;
+
+public class HomeController : AbpController
+{
+    public ActionResult Index()
+    {
+        return Redirect("~/swagger");
+    }
+}
---
+++ src/bookstore.HttpApi.Host/bookstoreBrandingProvider.cs
@@ -0,0 +1,10 @@
+﻿using Volo.Abp.DependencyInjection;
+using Volo.Abp.Ui.Branding;
+
+namespace bookstore;
+
+[Dependency(ReplaceServices = true)]
+public class bookstoreBrandingProvider : DefaultBrandingProvider
+{
+    public override string AppName => "bookstore";
+}
