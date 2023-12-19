fca41c844f60021918f614cd027b3b0075ce352c
Ali Mustafa <alimustafafortesting@gmail.com>
2023-12-19 10:02:01 +0200

    sixth one

---
+++ src/bookstore.DbMigrator/bookstoreDbMigratorModule.cs
@@ -0,0 +1,14 @@
+﻿using bookstore.EntityFrameworkCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace bookstore.DbMigrator;
+
+[DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(bookstoreEntityFrameworkCoreModule),
+    typeof(bookstoreApplicationContractsModule)
+    )]
+public class bookstoreDbMigratorModule : AbpModule
+{
+}
